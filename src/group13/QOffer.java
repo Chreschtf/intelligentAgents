197d0a5064a6c3db546a1031d892b269af851ecc
Frantzen Christian <frantzenchristian@hotmail.com>
2017-12-02 23:35:28 +0000

    updating nashQvalues

--- src/group13/QOffer.java
+++ src/group13/QOffer.java
@@ -5,15 +5,13 @@ public class QOffer {
     private Bid bid;
     private double utility;
     private double qvalue;
-    private double alpha;
-    private double gamma;
+    private final double alpha = 0.1;
+    private final double gamma = 0.9;
 
-    public QOffer(Bid bid_,double utility_,double qvalue_,double alpha_,double gamma_){
+    public QOffer(Bid bid_,double utility_,double qvalue_){
         bid=bid_;
         utility=utility_;
         qvalue=qvalue_;
-        gamma=gamma_;
-        alpha=alpha_;
     }
 
     public Bid getBid(){
